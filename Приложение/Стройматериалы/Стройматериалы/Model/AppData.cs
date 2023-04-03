1def359683301ed4a5f48ade0ef6687292555306
Dreamer-VD <danilavacun@gmail.com>
2023-04-04 02:45:09 +0300

    Added database and application

---
+++ Приложение/Стройматериалы/Стройматериалы/Model/AppData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Стройматериалы.Model
+{
+    public static class AppData
+    {
+        public static TradeDB db = new TradeDB();
+    }
+}
